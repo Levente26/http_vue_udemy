f2688fde400366271b3491068515aeb6458deff0
Levente26 <madi.levente26@gmail.com>
2022-04-26 21:02:17 +0200

    http request (get, post) error handling

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,9 +3,12 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found.</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -24,6 +27,44 @@ export default {
   // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    }
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+      fetch("https://http-vue-udemy-default-rtdb.europe-west1.firebasedatabase.app/surveys.json")
+        .then((response) => {
+          if(response.ok) {
+            return response.json();
+          }
+        }).then((data) => {
+          this.isLoading = false;
+          console.log(data);
+          const results = [];
+          for(const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        }).catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = "Failed to fetch data - please try again later";
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
